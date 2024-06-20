2bc044f584cf6a71b375e064f2a699fdf9e410b4
JHumbertoGL <kira.gnz1323@gmail.com>
2024-06-20 15:46:18 -0600

    feature: add validation for the form -humberto

--- SerieComponentsFront/src/Components/InputForm.jsx
+++ SerieComponentsFront/src/Components/InputForm.jsx
@@ -1,27 +1,45 @@
-
 import React, { useState } from 'react';
 
 const InputForm = ({ onSubmit }) => {
-    const [n, setN] = useState('');
+  const [n, setN] = useState('');
+  const [error, setError] = useState('');
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (n === '') {
+      setError('Please enter a value.');
+    } else if (isNaN(n) || n <= 0) {
+      setError('Porfavor ingresa numeros reales mayores a 0.');
+    } else {
+      setError('');
       onSubmit(n);
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="numero">Número "n" : </label>
-        <input
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (value === '' || !isNaN(value)) {
+      setN(value);
+      setError('');
+    } else {
+      setError('Porfavor ingresa numeros reales, no se aceptan letras o cualquier otro caracter.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="numero">Número "n" : </label>
+      <input
         className='pantalla'
-          type=""
-          id="numero"
-          value={n}
-          onChange={(e) => setN(e.target.value)}
-        />
-        <button className='button' type="submit">Calcular</button>
-      </form>
-    );
+        type="text"
+        id="numero"
+        value={n}
+        onChange={handleChange}
+      />
+      <button className='button' type="submit">Calcular</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
+  );
 };
 
 export default InputForm;
